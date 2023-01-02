9be2e972ccbb780dc88116a6ff5a491199ef0b03
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2023-01-02 15:32:17 +0900

    :tada:First Commit:tada:

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+
+require('dotenv').config();
+
+app.use(express.json());
+app.use('/', routes);
+
+const ErrorHandler = require('./middlewares/error.handler.middleware');
+app.use(ErrorHandler);
+
+app.listen(process.env.PORT, () => {
+    console.log(process.env.PORT, '포트로 서버가 열렸습니다.');
+});
+
+app.get('/', (req, res) => {
+    res.send(`BE ${process.env.PORT}`);
+});
