8b54fc702a2eb78c779484ba53e6f8807c78eb63
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2023-01-03 11:56:44 +0900

    :bug:fix : 모델 password추가

--- src/models/users.js
+++ src/models/users.js
@@ -37,6 +37,10 @@ module.exports = (sequelize, DataTypes) => {
         unique: true,
         allowNull: true,
       },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
       createdAt: {
         allowNull: false,
         type: DataTypes.DATE,
