4af5eb319d7a062a96a3e1cd86e02253dec3da3e
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2023-01-06 21:22:40 +0900

    :bug:fix : jwt 설치

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const loginMiddleware = require('../middlewares/login.middleware');
 
 router.use('/api/users/login', loginMiddleware, require('./login.route'));
-
-
 router.use('/api/products', require('./products.route'));
 router.use('/api/users', require('./users.route'));
 
