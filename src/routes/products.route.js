f45c60f4c6cb57becfa9a6ae9dd5e00ee158e4d4
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2023-01-06 21:58:30 +0900

    :bug:fix : 전체적인 에러 수정

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const loginMiddleware = require('../middlewares/login.middleware');
+const authMiddleware = require('../middlewares/authUser.middleware');
 
 const ProductController = require('../architecture/controllers/products.controller');
 const productController = new ProductController();
@@ -9,13 +9,13 @@ const productController = new ProductController();
 // api 저장하기
 router.get('/saved', productController.createProducts);
 // 저장(찜)한 제품 목록 가져오기
-router.get('/dibs', loginMiddleware, productController.getDibsProducts);
+router.get('/dibs', authMiddleware, productController.getDibsProducts);
 // 제품 비교하기
 router.get('/compare', productController.compareProducts);
 // 제품 목록 조회 (검색)
 router.get('/medicines', productController.findMedicines);
 // 제품 저장, 삭제하기 (찜하기)
-router.put('/:medicineId/dibs', loginMiddleware, productController.dibsProduct);
+router.put('/:medicineId/dibs', authMiddleware, productController.dibsProduct);
 // 제품 상세 조회
 router.get('/:medicineId', productController.findOneMedicine);
 
