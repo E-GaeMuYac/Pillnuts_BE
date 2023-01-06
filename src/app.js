d7c6751220e8722dd4baab6c01d6b0267a71b63d
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2023-01-06 17:00:22 +0900

    :bug:fix : cors 에러해결

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const cors = require('cors');
 
 require('dotenv').config();
 
 app.use(express.json());
 app.use('/', routes);
+app.use(cors());
 
 const ErrorHandler = require('./middlewares/error.handler.middleware');
 app.use(ErrorHandler);
