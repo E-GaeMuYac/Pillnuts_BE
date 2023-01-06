2412efd8680aac497921f271011144633f09e092
soogineer <ysooroh54@gmail.com>
2023-01-06 21:33:56 +0900

    :sparkles: authUser 파일에 refesh Token 검증 추가

--- src/middlewares/authUser.middleware.js
+++ src/middlewares/authUser.middleware.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const { AuthenticationError } = require("./exceptions/error.class");
+const { Users } = require('../models');
 
 module.exports = async (req, res, next) => {
     try {
@@ -7,35 +8,69 @@ module.exports = async (req, res, next) => {
         if (!req.headers.token) {
             throw new AuthenticationError("accessToken이 없습니다.", 404)
         };
-        const accessToken = req.headers.token;
-        // validateAccessToken() = 엑세스 토큰 확인
-        const isAccessTokenValidate = validateAccessToken(accessToken);
 
-        // AccessToken을 확인 했을 때 만료일 경우
-        if (!isAccessTokenValidate) {
-            throw new AuthenticationError("accessToken이 만료되었습니다.", 404)
+        const accessToken = req.headers.token;
+        const refreshToken = req.headers.token;
+
+        // access token, refresh token 확인
+        const isAccessTokenValidate = validateAccessToken(accessToken);
+        const isRefreshTokenValidate = validateRefreshToken(refreshToken);
+
+         // RefreshToken이 만료일 경우,
+         if (!isRefreshTokenValidate) {
+            throw new AuthenticationError("로그인이 유효하지 않습니다.",
+            401)
         };
+        
+        // AccessToken을 확인 했을 때 만료일 경우,
+        if (!isAccessTokenValidate) {
+            const user = await Users.findOne({
+                raw: true,
+                where: { refreshToken },
+                attribute: ['userId'],
+            });
+            if (!user) {
+                throw new AuthenticationError("로그인이 유효하지 않습니다.",
+                401)
+            };
+
+            // 새로운 Access token을 발급해준다.
+            const newAccessToken = createAccessToken(user.userId);
+            res.header('accesstoken', newAccessToken);
+            accesstoken = newAccessToken;
+        }
 
         const { userId } = getAccessTokenPayload(accessToken);
         res.locals.userId = userId;
         
         next();
     } catch (err) {
-        return res.status(400).json({ msg: "로그인이 필요합니다." });
+        next(err);
     }
 };
 
-// Access Token을 검증합니다.
+// Access Token을 검증한다.
 function validateAccessToken(accessToken) {
     try {
-        jwt.verify(accessToken, process.env.SECRET_KEY); // JWT를 검증합니다.
+        jwt.verify(accessToken, process.env.SECRET_KEY); // JWT를 검증한다.
         return true;
     } catch (error) {
         return false;
     }
 }
 
-// Access Token의 Payload를 가져옵니다.
+// Refresh Token을 검증한다.
+function validateRefreshToken(refreshToken) {
+    try {
+        jwt.verify(refreshToken, process.env.SECRET_KEY); // JWT를 검증한다.
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+
+// Access Token의 Payload를 가져온다.
 function getAccessTokenPayload(accessToken) {
     try {
         return jwt.verify(accessToken, process.env.SECRET_KEY); 
