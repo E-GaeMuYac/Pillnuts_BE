30dc33942e6d36eb2f2e22eafe7aee645fe9ca39
soogineer <ysooroh54@gmail.com>
2023-01-14 17:23:35 +0900

    :art: FIX : 불필요한 코드 삭제

--- src/passport/kakaoStrategy.js
+++ src/passport/kakaoStrategy.js
@@ -19,7 +19,6 @@ module.exports = () => {
       },
 
       async (request, accessToken, refreshToken, profile, done) => {
-        console.log(profile._json.kakao_account.profile.nickname)
         try {
           // 카카오 플랫폼에서 로그인 했었고 & 소셜 로그인 한 경우
           const KakaoExUser = await Users.findOne({
@@ -57,6 +56,7 @@ module.exports = () => {
             if (!nickname) {
               nickname = profile._json.kakao_account.email.split('@')[0];
             }
+
             // 가입되지 않은 유저면, 회원가입 시키고 로그인 시킨다.
             const KakaoNewUser = await Users.create({
               email: profile._json.kakao_account.email,
