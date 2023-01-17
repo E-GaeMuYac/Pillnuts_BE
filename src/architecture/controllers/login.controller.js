a7de2f5420e092be42e6ac38f8643717b784771e
soogineer <ysooroh54@gmail.com>
2023-01-17 14:03:30 +0900

    :art: FIX : 프론트 url에 맞게 변경

--- src/architecture/controllers/login.controller.js
+++ src/architecture/controllers/login.controller.js
@@ -67,7 +67,7 @@ class LoginController {
     const refreshtoken = req.user[1];
     const nickname = req.user[2];
     res.redirect(
-      `http://localhost:3000?accesstoken=${accesstoken}&refreshtoken=${refreshtoken}$nickname=${nickname}`
+      `http://localhost:3000/login/loading?accesstoken=${accesstoken}&refreshtoken=${refreshtoken}$nickname=${nickname}`
     ); 
     return;
   };
