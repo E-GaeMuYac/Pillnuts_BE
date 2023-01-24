cc8fc19bac38f418a74091b97cc1f1592d209e56
soogineer <ysooroh54@gmail.com>
2023-01-24 13:23:05 +0900

    :zap: FIX : body값 없을경우 에러 날려주기 추가

--- src/architecture/controllers/reviews.controller.js
+++ src/architecture/controllers/reviews.controller.js
@@ -1,4 +1,6 @@
 const ReviewService = require('../services/reviews.service');
+const { InvalidParamsError, } = require('../../middlewares/exceptions/error.class');
+const {AuthenticationError } = require('../../middlewares/authUser.middleware');
 
 class ReviewController {
   reviewService = new ReviewService();
@@ -8,9 +10,29 @@ class ReviewController {
       const { medicineId } = req.params;
       const { userId } = res.locals;
       const { review } = req.body;
-      
+
+      if(!review) {
+        throw new InvalidParamsError();
+      }
+
       await this.reviewService.createReview(medicineId, userId, review);
       res.status(200).json({ message: '댓글 등록이 완료되었습니다' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  findReview = async (req, res, next) => {
+    try {
+      const { medicineId, page, pageSize } = req.query;
+      const {refreshtoken} = req.headers;
+
+      if(!refreshtoken){
+        throw new AuthenticationError();
+      }
+      
+      await this.reviewsService.findReview(medicineId);
+      return res.status(200).json(medicineId);
     } catch (error) {
       next(error);
     }
