8022d6fadd07c6ccd6d541d27a7b65af664fa0fb
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2023-02-01 15:44:20 +0900

    :ambulance::fix : 토큰 유효시간 수정

--- src/util/token.js
+++ src/util/token.js
@@ -3,11 +3,11 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 createAccessToken = (userId) => {
-  return jwt.sign({ userId }, process.env.SECRET_KEY, { expiresIn: '2h' });
+  return jwt.sign({ userId }, process.env.SECRET_KEY, { expiresIn: '30s' });
 };
 
 createRefreshToken = () => {
-  return jwt.sign({}, process.env.SECRET_KEY, { expiresIn: '7d' });
+  return jwt.sign({}, process.env.SECRET_KEY, { expiresIn: '2m' });
 };
 
 createAuthToken = () => {
