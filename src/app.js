90145042a5349fb0c955a6fd85a6ba2bf1956ace
soogineer <ysooroh54@gmail.com>
2023-02-09 22:06:29 +0900

    :bulb: FIX: 불필요한 주석 삭제

--- src/app.js
+++ src/app.js
@@ -31,10 +31,8 @@ app.use(
   })
 );
 app.use(helmet());
-// express-session이 router 위에 와야함
-app.use(passport.initialize()); // 요청 객체에 passport 설정을 심음
-app.use(passport.session()); // req.session 객체에 passport 정보를 추가 저장
-// passport.session()이 실행되면, 세션쿠키 정보를 바탕으로 해서 passport/index.js의 deserializeUser()가 실행하게 됨
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
--- src/architecture/services/login.service.js
+++ src/architecture/services/login.service.js
@@ -14,22 +14,16 @@ class LoginService {
   loginRepository = new LoginRepository();
 
   existUser = async (email, password) => {
-    const existUser = await this.loginRepository.existUser(email); // 2. Users에 ExistUser의 email이 있는지 찾아본다.
+    const existUser = await this.loginRepository.existUser(email);
     if (!existUser) {
-      throw new ValidationError(
-        '로그인 정보를 다시 확인해주세요.', // 2-1. Users에 ExistUser의 email이 없다면, ExistError(412)를 띄운다.
-        412
-      );
+      throw new ValidationError('로그인 정보를 다시 확인해주세요.', 412);
     }
 
-    const match = compare(password, existUser.password); // 3. 비밀번호를 확인한다. -> existUser의 password만 불러온다.
+    const match = compare(password, existUser.password);
     if (!match) {
-      throw new ValidationError(
-        '로그인 정보를 다시 확인해주세요.', // 3-1. 입력한 비밀번호와 db에 저장되어있는 비번을 compare해서 비밀번호가 일치하지 않은 경우, validationError(412)를 띄운다.
-        412
-      );
+      throw new ValidationError('로그인 정보를 다시 확인해주세요.', 412);
     }
-    const accesstoken = await createAccessToken(existUser.userId); // 4. 비밀번호가 일치한다면, access token + refresh token을 발급해준다. (헤더(표준이라서..ㅎ*)에 넣어줘서 프론트로 보내준다.)
+    const accesstoken = await createAccessToken(existUser.userId);
     const refreshtoken = await createRefreshToken();
 
     const today = formatDate(new Date());
@@ -45,7 +39,7 @@ class LoginService {
       refreshtoken,
       loginCount
     );
-    return { accesstoken, refreshtoken, existUser }; // 5. 로그인 성공!
+    return { accesstoken, refreshtoken, existUser };
   };
 }
 
