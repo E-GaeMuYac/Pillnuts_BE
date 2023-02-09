3f96b871a8949d01081a08de114103a840693ed7
soogineer <ysooroh54@gmail.com>
2023-02-09 17:29:07 +0900

    :bulb: 불필요한 주석 삭제

--- swagger.js
+++ swagger.js
@@ -2,22 +2,22 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const options = {
   swaggerDefinition: {
-    openapi: '3.0.0', // 우리가 사용하고 있는 Open Api version
+    openapi: '3.0.0', 
     info: {
-      // API에 필요한 정보를 다루며 선택 필드
-      title: 'pillnuts', // API 제목
-      version: '1.0.0', // API version
-      description: 'a REST API using swagger and express.', // API 상세 정보
+      
+      title: 'pillnuts', 
+      version: '1.0.0', 
+      description: 'a REST API using swagger and express.', 
     },
     servers: [
       {
-        url: 'https://localhost:3000', // API에 대한 기본 URL을 정의하며, 배열로 여러 URL을 정의할 수 있음
+        url: 'https://localhost:3000', 
       },
     ],
   },
   apis: [],
 };
-const outputFile = './swagger-output.json' // swagger-autogen이 실행 후 생성될 파일 위치와 이름
-const endpointsFiles = ['./src/app.js'] // 읽어올 Router가 정의되어 있는  js파일들
+const outputFile = './swagger-output.json' 
+const endpointsFiles = ['./src/app.js'] 
 
 swaggerAutogen(outputFile, endpointsFiles, options);
